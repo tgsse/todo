9440e4d71b2520b1e0276e89bab9842e3287b7f0
tgsse <tamas.gura.sse@gmail.com>
2023-09-09 14:39:22 +0200

    Lint fixes

--- src/components/NewTask/TaskForm.js
+++ src/components/NewTask/TaskForm.js
@@ -1,26 +1,26 @@
-import { useRef } from 'react';
+import React, {useRef} from 'react'
 
-import classes from './TaskForm.module.css';
+import classes from './TaskForm.module.css'
 
 const TaskForm = (props) => {
-  const taskInputRef = useRef();
+    const taskInputRef = useRef()
 
-  const submitHandler = (event) => {
-    event.preventDefault();
+    const submitHandler = (event) => {
+        event.preventDefault()
 
-    const enteredValue = taskInputRef.current.value;
+        const enteredValue = taskInputRef.current.value
 
-    if (enteredValue.trim().length > 0) {
-      props.onEnterTask(enteredValue);
+        if (enteredValue.trim().length > 0) {
+            props.onEnterTask(enteredValue)
+        }
     }
-  };
 
-  return (
-    <form className={classes.form} onSubmit={submitHandler}>
-      <input type='text' ref={taskInputRef} />
-      <button>{props.loading ? 'Sending...' : 'Add Task'}</button>
-    </form>
-  );
-};
+    return (
+        <form className={classes.form} onSubmit={submitHandler}>
+            <input type="text" ref={taskInputRef}/>
+            <button>{props.loading ? 'Sending...' : 'Add Task'}</button>
+        </form>
+    )
+}
 
-export default TaskForm;
+export default TaskForm
